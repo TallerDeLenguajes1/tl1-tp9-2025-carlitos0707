52db8e65c32952508969f7f6a2b8eb921efabfd3
CARLOS URIBURU <carlosuriburupadilla@gmail.com>
2025-06-10 13:10:14 -0300

    Ejercicio 1

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Ingrese un directorio a analizar");
+string directorio = Console.ReadLine();
+
+
+while (!System.IO.Directory.Exists(directorio))
+{
+    Console.WriteLine("El directorio no existe");
+    directorio = Console.ReadLine();
+}
+
+string[] directorios = System.IO.Directory.GetDirectories(directorio);
+
+string[] archivos = System.IO.Directory.GetFiles(directorio);
+FileInfo[] infoarchivos = new FileInfo[archivos.Length];
+for (int i = 0; i < archivos.Length; i++)
+{
+    infoarchivos[i] = new FileInfo(archivos[i]);
+}
+
+Console.WriteLine($"*******************{directorio}*******************");
+
+for (int i = 0; i < directorios.Count(); i++)
+{
+    Console.WriteLine($"\t> {directorios[i]}");
+}
+for (int i = 0; i < archivos.Length; i++)
+{
+    Console.WriteLine($"\t {archivos[i]}   {infoarchivos[i].Length / 1000} KB");
+}
+
+FileStream archivo = new FileStream("reportes_archivos.csv", FileMode.Append);
+for (int i = 0; i < archivos.Length; i++)
+{
+    using (StreamWriter strwriter = new StreamWriter(archivo))
+    {
+        strwriter.Write($"Nombre: {infoarchivos[i].Name}  Tamaño (KB): {infoarchivos[i].Length}  Ultima Fecha de Modificacion: {infoarchivos[i].LastWriteTime} \n");
+        strwriter.Close();
+    }
+}
+
+
